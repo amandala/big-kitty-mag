98b2906c94364eecdd444a5ddf897f52a7aa1fad
Amanda Haynes <dev@portad.ca>
2020-09-09 20:16:54 -0600

    placeholder

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Head from "../components/head";
+import Nav from "../components/nav";
+
+import styles from "./index.module.scss";
+
+export default () => (
+  <div className={styles.Main}>
+    <Head title="Home" />
+    <img className={styles.Logo} src="/static/BKMagLogo_Pink.png" />
+    <h1 className={styles.ComingSoon}>Coming Soon</h1>
+  </div>
+);
