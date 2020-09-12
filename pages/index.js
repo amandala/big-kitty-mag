5b3cc5eb5298a9fc6dd66829aae542c89fa2096f
Amanda <amanda.haynes@versett.com>
2020-09-12 13:43:24 -0600

    layout tweaks and car cursor

--- pages/index.js
+++ pages/index.js
@@ -34,18 +34,24 @@ const Home = (props) => {
         </Link>
       </div>
       <div className={styles.FeatureStoryMobile}>
-        <img
-          className={styles.FeatureStoryMobileImage}
-          src={props.data["feature-story-image"].url}
-          alt={props.data["image-description"]}
-        />
+        <Link href={`/stories/${props.data.feature_story.uid}`}>
+          <div className={styles.HoverWrapper}>
+            <img
+              className={styles.FeatureStoryMobileImage}
+              src={props.data["feature-story-image"].url}
+              alt={props.data["image-description"]}
+            />
 
-        <div className={styles.FeatureStoryMobileDetails}>
-          <h1 className={styles.FeatureTitle}>
-            {props.data["feature-story-title"][0].text}
-          </h1>
-          <p>{props.data["feature-story-description"][0].text}</p>
-        </div>
+            <div className={styles.FeatureStoryMobileDetails}>
+              <H1 className={styles.FeatureTitle}>
+                {props.data.feature_story.data.title}
+              </H1>
+              <Body className={styles.Preview}>
+                {props.data.feature_story.data.preview[0].text}
+              </Body>
+            </div>
+          </div>
+        </Link>
       </div>
     </div>
   );
