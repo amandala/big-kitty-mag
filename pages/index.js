9fb7aa6d99db4da1adb07a2d35b372da8c56bed5
Amanda <amanda.haynes@versett.com>
2020-09-12 00:22:32 -0600

    export async w

--- pages/index.js
+++ pages/index.js
@@ -8,26 +8,26 @@ import styles from "./index.module.scss";
 
 import { Client } from "../prismic-configuration.js";
 
-const Home = ({ doc }) => {
-  console.log(JSON.stringify(doc));
+const Home = (props) => {
+  console.log(JSON.stringify(props));
   return (
     <div className={styles.Main}>
       <Head title="Home" />
       <div
         className={styles.FeatureStory}
         style={{
-          backgroundImage: `url(${doc.data["feature-story-image"].url})`,
+          backgroundImage: `url(${props.data["feature-story-image"].url})`,
         }}
       >
-        <Link href={`/stories/${doc.data.feature_story.uid}`}>
+        <Link href={`/stories/${props.data.feature_story.uid}`}>
           <div className={styles.FeatureWrapper}>
             <div className={styles.FeatureDetails}>
               <h4 className={styles.FeatureStoryHeading}>Feature Story</h4>
               <H1 className={styles.FeatureTitle}>
-                {doc.data.feature_story.data.title}
+                {props.data.feature_story.data.title}
               </H1>
               <Body className={styles.Preview}>
-                {doc.data.feature_story.data.preview[0].text}
+                {props.data.feature_story.data.preview[0].text}
               </Body>
             </div>
           </div>
@@ -36,28 +36,28 @@ const Home = ({ doc }) => {
       <div className={styles.FeatureStoryMobile}>
         <img
           className={styles.FeatureStoryMobileImage}
-          src={doc.data["feature-story-image"].url}
-          alt={doc.data["image-description"]}
+          src={props.data["feature-story-image"].url}
+          alt={props.data["image-description"]}
         />
 
         <div className={styles.FeatureStoryMobileDetails}>
           <h1 className={styles.FeatureTitle}>
-            {doc.data["feature-story-title"][0].text}
+            {props.data["feature-story-title"][0].text}
           </h1>
-          <p>{doc.data["feature-story-description"][0].text}</p>
+          <p>{props.data["feature-story-description"][0].text}</p>
         </div>
       </div>
     </div>
   );
 };
 
-Home.getStaticProps = async (ctx) => {
+export async getStaticProps = async (ctx) => {
   const req = ctx.req;
   const home = await Client(req).getSingle("home-page", {
     fetchLinks: ["article.title", "article.preview"],
   });
   return {
-    doc: home,
+    props: home,
   };
 };
 
