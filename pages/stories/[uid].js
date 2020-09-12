6fc606c3f660a949e1d3712bd8ac2f6dd8336008
Amanda <amanda.haynes@versett.com>
2020-09-12 00:10:39 -0600

    check for data before rendering

--- pages/stories/[uid].js
+++ pages/stories/[uid].js
@@ -9,41 +9,43 @@ const Story = (props) => {
 
   // console.log(doc.data.main_photo.dimensions);
 
-  return (
-    <div className={styles.Page}>
-      <main className={styles.Wrapper}>
-        <div className={styles.Header}>
-          <H1>{props.data.title}</H1>
-          <H5>{props.data.author.data.name}</H5>
-          <Meta>{props.data.released}</Meta>
-        </div>
-        <div>
-          <div className={styles.MainPhotoWrapper}>
-            <img
-              className={styles.Photo}
-              src={props.data.main_photo.url}
-              alt="Alt"
-            />
+  if (props.data) {
+    return (
+      <div className={styles.Page}>
+        <main className={styles.Wrapper}>
+          <div className={styles.Header}>
+            <H1>{props.data.title}</H1>
+            <H5>{props.data.author.data.name}</H5>
+            <Meta>{props.data.released}</Meta>
           </div>
           <div>
-            {props.data.story.map((s) => {
-              console.log(s);
-              if (s.type === "heading3") {
-                return <H3 className={styles.Heading}>{s.text}</H3>;
-              }
-              if (s.type === "heading2") {
-                return <H2 className={styles.Heading}>{s.text}</H2>;
-              }
-              if (s.type === "image") {
-                return <img className={styles.Photo} src={s.url} alt="Alt" />;
-              }
-              return <Body>{s.text}</Body>;
-            })}
+            <div className={styles.MainPhotoWrapper}>
+              <img
+                className={styles.Photo}
+                src={props.data.main_photo.url}
+                alt="Alt"
+              />
+            </div>
+            <div>
+              {props.data.story.map((s) => {
+                console.log(s);
+                if (s.type === "heading3") {
+                  return <H3 className={styles.Heading}>{s.text}</H3>;
+                }
+                if (s.type === "heading2") {
+                  return <H2 className={styles.Heading}>{s.text}</H2>;
+                }
+                if (s.type === "image") {
+                  return <img className={styles.Photo} src={s.url} alt="Alt" />;
+                }
+                return <Body>{s.text}</Body>;
+              })}
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
-  );
+        </main>
+      </div>
+    );
+  }
 };
 
 export const getServerSideProps = async (ctx) => {
