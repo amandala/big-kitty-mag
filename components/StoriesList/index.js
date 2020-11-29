d905f8b4ebd81c6093b3198df500cc68c5299e08
Amanda <amanda.haynes@versett.com>
2020-11-28 17:10:44 -0700

    tagging/filtering working

--- components/StoriesList/index.js
+++ components/StoriesList/index.js
@@ -4,45 +4,55 @@ import Header from "../Header";
 import styles from "./index.module.scss";
 
 const StoriesList = ({ stories, activeFilter }) => {
-  console.log({ activeFilter });
   return (
     <div className={styles.Page}>
-      {stories.results.map((story) => {
-        console.log({ story });
-        return (
-          <Link href={`/stories/${story.uid}`}>
-            <main className={styles.StoryWrapper}>
-              <div className={styles.StoryDetails}>
-                <div className={styles.Preview}>
-                  <H2>{story.data.title}</H2>
-                  <BodySmall> {story.data.deck}</BodySmall>
-                  <div className={styles.Tags}>
-                    {story.data.tags.map((tag) => {
-                      if (tag.tag.type === "tag") {
-                        return (
-                          <span
-                            className={styles.Tag}
-                            style={{ backgroundColor: tag?.tag?.data?.color }}
-                          >
-                            {tag?.tag?.data?.title}
-                          </span>
-                        );
-                      }
-                    })}
+      {stories.results
+        .filter((story) => {
+          console.log({ story });
+          if (activeFilter) {
+            return story.data.tags.find((tag) => {
+              if (tag.tag.data.title && tag.tag.data.title === activeFilter)
+                return story;
+            });
+          } else {
+            return story;
+          }
+        })
+        .map((story) => {
+          return (
+            <Link href={`/stories/${story.uid}`}>
+              <main className={styles.StoryWrapper}>
+                <div className={styles.StoryDetails}>
+                  <div className={styles.Preview}>
+                    <H2>{story.data.title}</H2>
+                    <BodySmall> {story.data.deck}</BodySmall>
+                    <div className={styles.Tags}>
+                      {story.data.tags.map((tag) => {
+                        if (tag.tag.type === "tag") {
+                          return (
+                            <span
+                              className={styles.Tag}
+                              style={{ backgroundColor: tag?.tag?.data?.color }}
+                            >
+                              {tag?.tag?.data?.title}
+                            </span>
+                          );
+                        }
+                      })}
+                    </div>
                   </div>
+                  <Meta className={styles.ReadMore}> Keep Reading</Meta>
                 </div>
-                <Meta className={styles.ReadMore}> Keep Reading</Meta>
-              </div>
-              <div className={styles.ImageWrapper}>
-                <img
-                  className={styles.StoryPhoto}
-                  src={story.data.main_photo.url}
-                />
-              </div>
-            </main>
-          </Link>
-        );
-      })}
+                <div className={styles.ImageWrapper}>
+                  <img
+                    className={styles.StoryPhoto}
+                    src={story.data.main_photo.url}
+                  />
+                </div>
+              </main>
+            </Link>
+          );
+        })}
     </div>
   );
 };
