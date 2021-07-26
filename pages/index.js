5f25a28b1a85c81ced36244743966c63e713ba73
Amanda Haynes <dev@portad.ca>
2021-07-26 16:57:21 -0600

    search value to lovwe

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ const Home = ({ home, stories, ads, tags }) => {
             activeFilter ? activeFilter : "All"
           } Stories`}</H1>
           <div className={styles.SearchBox}>
-            <input className={styles.Search} value={searchTerm} onChange={(d) => setSearchTerm(d.target.value)} />
+            <input className={styles.Search} value={searchTerm} onChange={(d) => setSearchTerm(d.target.value.toLowerCase())} />
             <img className={styles.SearchIcon} src="/searchIcon.svg" />
           </div>
            <div className={styles.SearchBoxMobile}>
